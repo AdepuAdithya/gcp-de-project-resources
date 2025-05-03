fd1653e6879055f31fe5fe5e755ce42125bf3ea7
AdepuAdithya <adithya.adepu@gmail.com>
2025-05-03 20:54:13 +0530

    Added Curation dataset and tables

--- bld/main.tf
+++ bld/main.tf
@@ -7,6 +7,7 @@ resource "google_bigquery_dataset" "raw_dataset" {
     prevent_destroy = false
   }
 }
+
 # Raw Tables
 resource "google_bigquery_table" "Department_raw_table" {
   dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
@@ -46,7 +47,7 @@ resource "google_bigquery_table" "EmployeePayHistory_raw_table" {
 }
 
 
-# Staging Layer
+# Staging Dataset
 resource "google_bigquery_dataset" "staging_dataset" {
   dataset_id = "Employee_Details_stg"
   location   = var.dataset_location
@@ -56,6 +57,7 @@ resource "google_bigquery_dataset" "staging_dataset" {
   }
 }
 
+#Staging Tables
 resource "google_bigquery_table" "Department_stg_table" {
   dataset_id = google_bigquery_dataset.staging_dataset.dataset_id
   table_id   = "Department_stg"
@@ -94,49 +96,23 @@ resource "google_bigquery_table" "EmployeePayHistory_stg_table" {
 }
 
 
-# Curation Layer
-# resource "google_bigquery_dataset" "curation_dataset" {
-#   dataset_id = "employee_details_cur"
-#   location   = var.dataset_location
+# Curation Dataset
+resource "google_bigquery_dataset" "curation_dataset" {
+  dataset_id = "employee_details_cur"
+  location   = var.dataset_location
 
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
 
-# resource "google_bigquery_table" "curation_table" {
-#   dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
-#   table_id   = "Department_cur"
-#   schema     = file("C:/Users/adith/Documents/GitHub/My_Project_Resources/curation/department_curation.json") # Reusing the same schema file
+# Curation Table
+resource "google_bigquery_table" "curation_table" {
+  dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
+  table_id   = "EmployeeDepartmentAndPayHistory_cur"
+  schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/curation/EmployeeDepartmentAndPayHistory_cur.json") # Reusing the same schema file
 
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
-# resource "google_bigquery_table" "curation_table" {
-#   dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
-#   table_id   = "employee_cur"
-#   schema     = file("C:/Users/adith/Documents/GitHub/My_Project_Resources/curation/employee_curation.json") # Reusing the same schema file
-
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
-# resource "google_bigquery_table" "curation_table" {
-#   dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
-#   table_id   = "employeedepartmenthistory_cur"
-#   schema     = file("C:/Users/adith/Documents/GitHub/My_Project_Resources/curation/employeedepartmenthistory_curation.json") # Reusing the same schema file
-
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
-# resource "google_bigquery_table" "curation_table" {
-#   dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
-#   table_id   = "employeepayhistory_cur"
-#   schema     = file("C:/Users/adith/Documents/GitHub/My_Project_Resources/curation/employeepayhistory_curation.json") # Reusing the same schema file
-
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
