4bb411db260923ea7d1de0e5772126d44bbc4a9f
AdepuAdithya <adithya.adepu@gmail.com>
2025-05-05 19:28:02 +0530

    Added consumption views and metadata dataset and table

--- bld/main.tf
+++ bld/main.tf
@@ -13,6 +13,7 @@ resource "google_bigquery_table" "Department_raw_table" {
   dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
   table_id   = "Department_raw"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/raw/Department_raw.json") # Path to the JSON schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -22,6 +23,7 @@ resource "google_bigquery_table" "Employee_raw_table" {
   dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
   table_id   = "Employee_raw"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/raw/Employee_raw.json") # Path to the JSON schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -31,6 +33,7 @@ resource "google_bigquery_table" "EmployeeDepartmentHistory_raw_table" {
   dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
   table_id   = "EmployeeDepartmentHistory_raw"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/raw/EmployeeDepartmentHistory_raw.json") # Path to the JSON schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -40,6 +43,7 @@ resource "google_bigquery_table" "EmployeePayHistory_raw_table" {
   dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
   table_id   = "EmployeePayHistory_raw"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/raw/EmployeePayHistory_raw.json") # Path to the JSON schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -62,6 +66,7 @@ resource "google_bigquery_table" "Department_stg_table" {
   dataset_id = google_bigquery_dataset.staging_dataset.dataset_id
   table_id   = "Department_stg"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/staging/Department_stg.json") # Reusing the same schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -71,6 +76,7 @@ resource "google_bigquery_table" "Employee_stg_table" {
   dataset_id = google_bigquery_dataset.staging_dataset.dataset_id
   table_id   = "Employee_stg"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/staging/Employee_stg.json") # Reusing the same schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -80,6 +86,7 @@ resource "google_bigquery_table" "EmployeeDepartmentHistory_stg_table" {
   dataset_id = google_bigquery_dataset.staging_dataset.dataset_id
   table_id   = "EmployeeDepartmentHistory_stg"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/staging/EmployeeDepartmentHistory_stg.json") # Reusing the same schema file
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
@@ -90,6 +97,7 @@ resource "google_bigquery_table" "EmployeePayHistory_stg_table" {
   table_id   = "EmployeePayHistory_stg"
   schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/staging/EmployeePayHistory_stg.json") # Reusing the same schema file
   deletion_protection = false
+
   lifecycle {
     prevent_destroy = false
   }
@@ -103,5 +111,102 @@ resource "google_bigquery_dataset" "curation_dataset" {
 
   lifecycle {
     prevent_destroy = false
+  }
+}
+
+# Curation Tables
+resource "google_bigquery_table" "curation_table" {
+  dataset_id = google_bigquery_dataset.curation_dataset.dataset_id
+  table_id   = "EmployeeDepartmentAndPayHistory_cur"
+  schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/curation/EmployeeDepartmentAndPayHistory_cur.json") # Path to the JSON schema file
+  deletion_protection = false
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+#ingestion metadata dataset
+resource "google_bigquery_dataset" "metadata_dataset" {
+  dataset_id    = "Ingestion_Metadata"
+  description   = "Stores metadata related to data ingestion events"
+  location      = var.dataset_location
+}
+
+#ingestion metadata table
+resource "google_bigquery_table" "metadata_ingestion_log" {
+  dataset_id = google_bigquery_dataset.metadata_dataset.dataset_id
+  table_id   = "IngestionMetadataLogs"
+  schema     = file("C:/Users/adith/Documents/GitHub/gcp-de-project-resources/bld/metadata/IngestionMetadataLogs.json") # Path to the JSON schema file
+  deletion_protection = false
+
+  lifecycle {
+  prevent_destroy = false
+  }
+}
+
+#Consumption Dataset
+# This dataset is for analytical consumption views
+
+resource "google_bigquery_dataset" "analytics_dataset" {
+  dataset_id    = "Employee_Analytics"
+  description   = "Dataset for analytical consumption views"
+  location      = var.dataset_location
+}
+#consumption Views
+resource "google_bigquery_table" "vw_WorkforceDemographics" {
+  dataset_id = google_bigquery_dataset.analytics_dataset.dataset_id
+  table_id   = "vw_WorkforceDemographics"
+  
+  view {
+    query          = <<EOF
+    SELECT BusinessEntityID, JobTitle, DepartmentName, Gender, Age, TenureYears, IsActive
+    FROM `gcp-de-batch-sim-458416.Employee_Details_cur.EmployeeDepartmentAndPayHistory_cur`
+    EOF
+    use_legacy_sql = false
+  }
+}
+
+resource "google_bigquery_table" "vw_CompensationTrends" {
+  dataset_id = google_bigquery_dataset.analytics_dataset.dataset_id
+  table_id   = "vw_CompensationTrends"
+
+  view {
+    query          = <<EOF
+    SELECT BusinessEntityID, JobTitle, DepartmentName, Rate AS CurrentPayRate, 
+           PayFrequency, YearsSinceRateChange, RateChangeImpact, HourlyRate
+    FROM `gcp-de-batch-sim-458416.Employee_Details_cur.EmployeeDepartmentAndPayHistory_cur`
+    EOF
+    use_legacy_sql = false
+  }
+}
+
+resource "google_bigquery_table" "vw_DepartmentStabilityMobility" {
+  dataset_id = google_bigquery_dataset.analytics_dataset.dataset_id
+  table_id   = "vw_DepartmentStabilityMobility"
+
+  view {
+    query          = <<EOF
+    SELECT BusinessEntityID, JobTitle, DepartmentID, DepartmentName, StartDate AS DepartmentStartDate, 
+           DepartmentTenureYears, EndDate AS DepartmentExitDate, IsActive
+    FROM `gcp-de-batch-sim-458416.Employee_Details_cur.EmployeeDepartmentAndPayHistory_cur`
+    EOF
+    use_legacy_sql = false
+  }
+}
+
+resource "google_bigquery_table" "vw_PayrollForecasting" {
+  dataset_id = google_bigquery_dataset.analytics_dataset.dataset_id
+  table_id   = "vw_PayrollForecasting"
+
+  view {
+    query          = <<EOF
+    SELECT DepartmentName, COUNT(BusinessEntityID) AS EmployeeCount, 
+           SUM(HourlyRate * PayFrequency) AS TotalPayrollCost, 
+           AVG(HourlyRate) AS AvgHourlyPayRate
+    FROM `gcp-de-batch-sim-458416.Employee_Details_cur.EmployeeDepartmentAndPayHistory_cur`
+    WHERE IsActive = TRUE
+    GROUP BY DepartmentName
+    EOF
+    use_legacy_sql = false
   }
 }
